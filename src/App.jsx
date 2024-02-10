fb8bc886542dc5f317d513c24e4e6657d02d117a
Rokas <rokas.vaitkevicius1@gmail.com>
2024-02-10 16:15:31 +0200

    Added state managment with context api to compound components

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ function App() {
     <section>
       <h2>Why work with us</h2>
       <Accordion className='accordion'>
-        <AccordionItem title="We got 20 years of experience" className="accordion-item">
+        <AccordionItem id="experience" title="We got 20 years of experience" className="accordion-item">
           <article>
             <p>Cant go wrong with us</p>
             <p>We plan the best holiday trips</p>
           </article>
         </AccordionItem>
-        <AccordionItem title="We work with local guides" className="accordion-item">
+        <AccordionItem id="local-guides" title="We work with local guides" className="accordion-item">
           <article>
             <p>We work with the best of the best</p>
             <p>We work with locals</p>
